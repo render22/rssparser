c1c29a6ca6c045147fe8a61d068fd4eed8eb1dd4
Admin <kotovviktor@yahoo.com>
2014-10-06 00:17:53 +0300

    change 5

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ define("CONFIG_DIR", __DIR__ . "/config");
 define("ENGINE_DIR", __DIR__ . "/engine");
 define("TEMPLATES_DIR", __DIR__ . "/templates");
 require_once __DIR__ . "/engine/autoloader.php";
-Application\Application::run(  );
+Application\Application::run();
 
 Application\Application::run();
 $a;
+$d;
 ?>
